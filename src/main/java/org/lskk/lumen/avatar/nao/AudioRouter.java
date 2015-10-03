fb2eb88f76f9c3027f09e9f53acd1f1d55674240
Hendy Irawan <ceefour666@gmail.com>
2015-10-03 19:36:18 +0700

    play using remote buffer

--- src/main/java/org/lskk/lumen/avatar/nao/AudioRouter.java
+++ src/main/java/org/lskk/lumen/avatar/nao/AudioRouter.java
@@ -1,14 +1,8 @@
 package org.lskk.lumen.avatar.nao;
 
 import com.aldebaran.proxy.*;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.github.ooxi.jdatauri.DataUri;
-import com.google.common.base.Preconditions;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
 import org.lskk.lumen.core.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +10,13 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.ByteArrayInputStream;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
@@ -37,9 +36,86 @@ public class AudioRouter extends RouteBuilder {
     @Inject
     private ToJson toJson;
 
+    public static class AudioData {
+        private AudioFormat format;
+        private byte[] data;
+
+        public AudioData(AudioFormat format, byte[] data) {
+            this.format = format;
+            this.data = data;
+        }
+
+        public AudioFormat getFormat() {
+            return format;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
+    }
+
+    public static AudioData convertAudioData(byte[] sourceBytes, AudioFormat audioFormat) throws IOException, UnsupportedAudioFileException {
+        if (sourceBytes == null || sourceBytes.length == 0 || audioFormat == null) {
+            throw new IllegalArgumentException("Illegal Argument passed to this method");
+        }
+
+        try (final ByteArrayInputStream bais = new ByteArrayInputStream(sourceBytes);
+             final AudioInputStream sourceAIS = AudioSystem.getAudioInputStream(bais)) {
+            final AudioFormat sourceFormat = sourceAIS.getFormat();
+            final AudioFormat convertFormat = new AudioFormat(
+                    AudioFormat.Encoding.PCM_SIGNED, sourceFormat.getSampleRate(), 16,
+                    sourceFormat.getChannels(), sourceFormat.getChannels() * 2, sourceFormat.getSampleRate(), false);
+            try (final AudioInputStream convert1AIS = AudioSystem.getAudioInputStream(convertFormat, sourceAIS);
+                 final AudioInputStream convert2AIS = AudioSystem.getAudioInputStream(audioFormat, convert1AIS);
+                 final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                byte[] buffer = new byte[8192];
+                while (true) {
+                    int readCount = convert2AIS.read(buffer, 0, buffer.length);
+                    if (readCount == -1) {
+                        break;
+                    }
+                    baos.write(buffer, 0, readCount);
+                }
+                return new AudioData(audioFormat, baos.toByteArray());
+            }
+        }
+    }
+
+    public static AudioData getAudioDataPcmSigned(byte[] sourceBytes) throws IOException, UnsupportedAudioFileException {
+        if (sourceBytes == null || sourceBytes.length == 0) {
+            throw new IllegalArgumentException("Illegal Argument passed to this method");
+        }
+
+        try (final ByteArrayInputStream bais = new ByteArrayInputStream(sourceBytes);
+             final AudioInputStream sourceAIS = AudioSystem.getAudioInputStream(bais)) {
+            final AudioFormat sourceFormat = sourceAIS.getFormat();
+            final AudioFormat convertFormat = new AudioFormat(
+                    AudioFormat.Encoding.PCM_SIGNED, sourceFormat.getSampleRate(), 16,
+                    sourceFormat.getChannels(), sourceFormat.getChannels() * 2, sourceFormat.getSampleRate(), false);
+            try (final AudioInputStream convert1AIS = AudioSystem.getAudioInputStream(convertFormat, sourceAIS);
+                 final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                byte[] buffer = new byte[8192];
+                while (true) {
+                    int readCount = convert1AIS.read(buffer, 0, buffer.length);
+                    if (readCount == -1) {
+                        break;
+                    }
+                    baos.write(buffer, 0, readCount);
+                }
+                return new AudioData(convertFormat, baos.toByteArray());
+            }
+        }
+    }
+
     @Override
     public void configure() throws Exception {
         final String naoPassword = env.getRequiredProperty("nao.password");
+        // NAO only supports limited sample rates, so we limit to 22050 Hz (default)
+        final int sampleRate = 22050;
+        final int channelCount = 2;
+        final AudioFormat naoFormat = new AudioFormat(
+                AudioFormat.Encoding.PCM_SIGNED, sampleRate, 16,
+                channelCount, channelCount * 2, sampleRate, false);
         from("rabbitmq://localhost/amq.topic?connectionFactory=#amqpConnFactory&exchangeType=topic&autoDelete=false&routingKey=avatar.nao1.audio.out&concurrentConsumers=4")
                 .to("log:IN.avatar.nao1.audio.out?showHeaders=true&showAll=true&multiline=true")
                 .process(exchange -> {
@@ -56,33 +132,40 @@ public class AudioRouter extends RouteBuilder {
                             log.info("Played locally: {}", contentUrl.getPath());
                         } else if ("data".equals(contentUrl.getScheme())) {
                             final DataUri dataUri = DataUri.parse(playAudio.getContentUrl(), StandardCharsets.UTF_8);
-//                            final File audioFile = File.createTempFile("nao_", ".mp3");
-                            try {
-//                                log.info("Writing {} bytes to {} ...", dataUri.getData().length, audioFile);
-//                                FileUtils.writeByteArrayToFile(audioFile, dataUri.getData());
-                                final String destFile = "/home/nao/tmp.mp3";
-                                final String naoUser = "nao";
-                                log.info("Uploading {} bytes to ftp://{}@{}{} ...", dataUri.getData().length, naoUser, naoConfig.getNaoHost(), destFile);
-                                final FTPClient ftpClient = new FTPClient();
-                                ftpClient.connect(naoConfig.getNaoHost());
-                                try {
-                                    Preconditions.checkArgument(ftpClient.login(naoUser, naoPassword),
-                                            "Cannot connect to FTP {} using user '{}'", naoConfig.getNaoHost(), naoUser);
-                                    ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-                                    try (ByteArrayInputStream bais = new ByteArrayInputStream(dataUri.getData())) {
-                                        Preconditions.checkArgument(ftpClient.storeFile(FilenameUtils.getName(destFile), bais),
-                                                "Cannot upload {} bytes to ftp://{}@{}{}", dataUri.getData().length,
-                                                naoUser, naoConfig.getNaoHost(), destFile);
-                                    }
-                                } finally {
-                                    ftpClient.disconnect();
-                                }
-                                log.info("Playing uploaded file locally: {}", destFile);
-                                audioPlayer.playFile(destFile);
-                                log.info("Played uploaded file locally: {}", destFile);
-                            } finally {
-//                                audioFile.delete();
-                            }
+
+//                            final String destFile = "/home/nao/tmp.mp3";
+//                            final String naoUser = "nao";
+//                            log.info("Uploading {} bytes to ftp://{}@{}{} ...", dataUri.getData().length, naoUser, naoConfig.getNaoHost(), destFile);
+//                            final FTPClient ftpClient = new FTPClient();
+//                            ftpClient.connect(naoConfig.getNaoHost());
+//                            try {
+//                                Preconditions.checkArgument(ftpClient.login(naoUser, naoPassword),
+//                                        "Cannot connect to FTP {} using user '{}'", naoConfig.getNaoHost(), naoUser);
+//                                ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+//                                try (ByteArrayInputStream bais = new ByteArrayInputStream(dataUri.getData())) {
+//                                    Preconditions.checkArgument(ftpClient.storeFile(FilenameUtils.getName(destFile), bais),
+//                                            "Cannot upload {} bytes to ftp://{}@{}{}", dataUri.getData().length,
+//                                            naoUser, naoConfig.getNaoHost(), destFile);
+//                                }
+//                            } finally {
+//                                ftpClient.disconnect();
+//                            }
+//                            log.info("Playing uploaded file locally: {}", destFile);
+//                            audioPlayer.playFile(destFile);
+//                            log.info("Played uploaded file locally: {}", destFile);
+
+                            // using remotebuffer
+                            final AudioData naoSound = convertAudioData(dataUri.getData(), naoFormat);
+                            final Variant fStereoAudioData = new Variant(naoSound.getData());
+                            final int frameCount = naoSound.getData().length / naoSound.getFormat().getFrameSize();
+                            log.info("Playing converted file from {} bytes MP3 to {} bytes WAV in {} frames: {}",
+                                    dataUri.getData().length, naoSound.getData().length,
+                                    frameCount, naoSound.getFormat());
+//                            audioDevice.setParameter("outputSampleRate", (int) naoSound.getFormat().getSampleRate());
+                            audioDevice.sendRemoteBufferToOutput(frameCount, fStereoAudioData);
+                            log.info("Played converted file from {} bytes MP3 to {} bytes WAV in {} frames: {}",
+                                    dataUri.getData().length, naoSound.getData().length,
+                                    frameCount, naoSound.getFormat());
                         } else {
                             throw new NaoException("Unknown audio URL: " + contentUrl);
                         }
