fde11ce09af8fa53243b64447f30720255836d74
Lumen RabbitMQ <ceefour666@gmail.com>
2015-10-06 11:41:45 +0700

    avatar

--- avatar-nao.sh
+++ avatar-nao.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname $0)"
-java -cp $SCRIPT_DIR'/target/dependency/*':$SCRIPT_DIR'/target/classes' id.ac.itb.lumen.avatar.nao.AvatarNaoApp "$@"
+java -cp $SCRIPT_DIR'/target/dependency/*':$SCRIPT_DIR'/target/classes' org.lskk.lumen.avatar.nao.AvatarNaoApp "$@"
