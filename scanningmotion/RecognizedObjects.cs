f44b75d95b374a378d3a95ff204028d953f9f46e
irfansimamora <irfansimamora10@yahoo.co.id>
2015-11-28 02:02:49 +0700

    Scanning Motion

---
+++ scanningmotion/RecognizedObjects.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace naoTest
+{
+    class RecognizedObjects
+    {
+        [JsonProperty("@type")]
+        public String type = "RecognizedObjects";
+        bool hasPosition = true;
+        bool hasDistance = false;
+        bool hasYaw = false;
+        public List<RecognizedObject> trashes = new List<RecognizedObject>();
+        public List<RecognizedObject> trashCans = new List<RecognizedObject>();
+
+        public override string ToString()
+        {
+            return ""+ string.Join(", ", trashes)+"";
+        }
+    }
+}
