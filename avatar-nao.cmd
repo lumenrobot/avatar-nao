02325b81f7f8c174498c30670694086d35733b04
Hendy Irawan <ceefour666@gmail.com>
2015-12-20 20:00:54 +0700

    update cmd

--- avatar-nao.cmd
+++ avatar-nao.cmd
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files (x86)\Java\jdk1.8.0_66\bin\java" -Xms256m -Xmx256m -cp target/dependency/*;target/classes org.lskk.lumen.avatar.nao.AvatarNaoApp %*
+"C:\Program Files (x86)\Java\jdk1.8.0_66\bin\java" -Xms256m -Xmx256m -Djava.awt.headless=true -cp target/dependency/*;target/classes org.lskk.lumen.avatar.nao.AvatarNaoApp %*
